fddbb945f20f402f4f7edb2e9444cbf5336f43e8
patocox <patocox@users.noreply.github.com>
2015-07-10 11:30:00 -0500

    Update docker-machine-do.sh

--- docker-machine-do.sh
+++ docker-machine-do.sh
@@ -31,8 +31,9 @@ docker-machine --debug create \
   swarm-master
 
 #Create Swarm Nodes and configure
-for i in $(seq 1 $SWARM_NODES); do
-	docker-machine create \
+SWARM_NODES=$SWARM_NODES-1
+for i in $(seq 0 $SWARM_NODES); do
+	docker-machine --debug create \
 	  --driver digitalocean \
 	  --digitalocean-access-token $API_TOKEN \
 	  --digitalocean-private-networking \
