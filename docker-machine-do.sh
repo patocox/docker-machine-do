fef19ab6b0dddf431319d508241726ba0eb9b4f2
patocox <patocox@users.noreply.github.com>
2015-09-23 08:22:48 -0500

    Update docker-machine-do.sh

--- docker-machine-do.sh
+++ docker-machine-do.sh
@@ -41,38 +41,3 @@ for i in $(seq 1 $SWARM_NODES); do
 	  swarm-node-$i
 done
 
-#Switch to Swarm-master
-eval "$(docker-machine env --swarm swarm-master)"
-
-#Create Docker-Compose yml
-echo '---
-HAPROXY:
-  image: bfirsh/interlock-haproxy-swarm
-  ports:
-  - 80:8080
-  environment:
-  - DOCKER_HOST
-  - constraint:node==swarm-master
-  volumes:
-  - /etc/docker:/etc/docker
-WP:
-  image: centurylink/wordpress:3.9.1
-  ports:
-  - 8080:80
-  links:
-  - DB:DB_1
-  environment:
-  - DB_PASSWORD=pass@word01
-  - DB_NAME=wordpress
-  - affinity:container!=~WP*
-  - affinity:container!=~DB*
-  hostname: wordpress.local
-DB:
-  image: centurylink/mysql:5.5
-  ports:
-  - 3306:3306
-  environment:
-  - MYSQL_ROOT_PASSWORD=pass@word01' > docker-compose.yml
-
-# Run docker-compose.yml
-docker-compose scale HAPROXY=1 WP=3 DB=1
